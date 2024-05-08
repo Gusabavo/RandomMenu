af2252a301949b3a64dcf9e8ccae24b42acd6f32
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-05-08 17:25:37 -0300

    MainWindows.xaml.cs implemented.

--- RandomMenu/MenuItem.cs
+++ RandomMenu/MenuItem.cs
@@ -9,7 +9,7 @@ namespace RandomMenu
 {
     class MenuItem
     {
-        public Random Randomizer = new Random();
+        public static Random Randomizer = new Random();
 
         public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
         public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
@@ -27,7 +27,7 @@ namespace RandomMenu
 
             decimal bucks = Randomizer.Next(2, 5);
             decimal cents = Randomizer.Next(1, 98);
-            decimal price = bucks + (cents * 01M);
+            decimal price = bucks + (cents * .01M);
             Price = price.ToString("c");
         }
     }
