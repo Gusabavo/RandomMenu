d8253749c58f949741ec91fbc9b8eee5b88b6f27
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-05-08 15:43:49 -0300

    Adicionar arquivos de projeto.

---
+++ RandomMenu/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RandomMenu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
